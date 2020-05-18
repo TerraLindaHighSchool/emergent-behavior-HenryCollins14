2cb818a19c1be75fc62c83820a26bb509db998a8
HenryCollins14 <60672325+HenryCollins14@users.noreply.github.com>
2020-05-17 22:53:06 -0700

    Project Complete

--- ants/Ant.java
+++ ants/Ant.java
@@ -11,8 +11,11 @@ public class Ant extends Creature
     private boolean carryingFood = false;
     private GreenfootImage image1;
     private GreenfootImage image2;
-    
-     /**
+    private final int MAX_PH_AVAILABLE = 16;
+    private final int TIME_FOLLOWING_TRAIL = 30;
+    private int phAvailable = MAX_PH_AVAILABLE;
+    private int followTrailTimeRemaining = 0;
+    /**
      * Create an ant with a given home hill. The initial speed is zero (not moving).
      */
     public Ant(AntHill home)
@@ -21,7 +24,6 @@ public class Ant extends Creature
         image1 = getImage();
         image2 = new GreenfootImage("ant-with-food.gif");
     }
-    
     /**
      * Do what an ant's gotta do.
      */
@@ -29,41 +31,47 @@ public class Ant extends Creature
     {
         status();
     }
-    
     private void checkForFood()
     {
         Food food = (Food) getOneIntersectingObject(Food.class);
         if (food != null) 
         {
-            food.removeCrumb();
             carryingFood = true;
+            food.removeCrumb();
             setImage(image2);
         }
     }
-    
     private boolean atHome()
     {
-        if (getHomeHill() !=null)
+        if(getHomeHill() != null)
         {
-            return (Math.abs(getX() - getHomeHill().getX()) < 4) &&
-                   (Math.abs(getY() - getHomeHill().getY()) < 4);
-        }
+           return (Math.abs(getX() - getHomeHill().getX()) < 4) && 
+                  (Math.abs(getY() - getHomeHill().getY()) < 4);
+        }  
         else
         {
             return false;
         }
     }
-    
     private void searchForFood()
     {
-        randomWalk();
+        if(followTrailTimeRemaining == 0)
+        {
+         walkTowardsPheromoneCenter();
+         randomWalk();
+        }
+        else
+        {
+            followTrailTimeRemaining--;
+            walkAwayFromHome();
+        }
         checkForFood();
     }
-    
     private void status()
     {
-        if (carryingFood == true)
+        if(carryingFood == true)
         {
+            handlePheromoneDrop();
             walkTowardsHome();
             if(atHome())
             {
@@ -72,10 +80,50 @@ public class Ant extends Creature
                 getHomeHill().countFood();
             }
         }
-        else
+         else
         {
             searchForFood();
         }
     }
+    private void handlePheromoneDrop()
+    {
+        if(MAX_PH_AVAILABLE == 16)
+        {
+           Pheromone ph = new Pheromone();
+           getWorld().addObject(ph, getX(),getY());
+           phAvailable = 0;
+        }
+        else
+        {
+            phAvailable++;
+        }
+        status();
+    }
+    
+    public boolean smellsPheromone()
+    {
+       if(isTouching(Pheromone.class))
+       {
+          return true;   
+       }
+        else
+       {
+          return false;
+       }
+   }
+   private void walkTowardsPheromoneCenter()
+   {
+       Pheromone pheromone = (Pheromone) getOneIntersectingObject(Pheromone.class);
+       if (pheromone != null)
+       {
+          headTowards(pheromone);
+           if (getX() == pheromone.getX() && getY() ==pheromone.getY())
+          {
+            followTrailTimeRemaining = TIME_FOLLOWING_TRAIL;    
+          }
+        }
+   } 
 }
+
+
 
